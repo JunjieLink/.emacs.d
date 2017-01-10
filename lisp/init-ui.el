82ab02a3f0141bfbdc0b3eb46dc9229da8f70938
Junjie <liang@junjie.link>
2017-01-10 15:44:37 +0800

    global-linum-mode off

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,7 +1,6 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-(global-linum-mode 1)
 (setq-default cursor-type 'bar)
 (setq inhibit-splash-screen 1)
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
